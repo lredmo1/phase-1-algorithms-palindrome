8fa2dc4414e24998f940bc2ddebc89273ec71b2c
Lisa <redlisa125@gmail.com>
2021-10-13 14:36:54 -1000

    commit tree b18c3bfe6c988b3d3fdef08c8474c000b318cbc9

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
+// Write a function that determines if a word is the same letters forward and backward.
+// Try racecar, mom, and Lisa
 
-/* 
-  Add your pseudocode here
-*/
+
+
+function isPalindrome(word) {
+  let lettersArray = word.split("")
+  let reverseLettersArray = lettersArray.reverse()
+  let newWord = reverseLettersArray.join("")
+  return (word === newWord ? true : false)
+ }
+
+
+
+// The function will take in a word and split it up by letter. Then it will reverse the letters in the array and rejoin them back together. This will be set to a new variable name and compared to the original word. If they are equal the word function will return true. If they are not the same, it will return false.
 
 /*
   Add written explanation of your solution here
+  //I'm not sure what this means.
 */
 
 // You can run `node index.js` to view these console logs
